0b124de2b36b55cb5697f2f8b3f800c108cded90
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-02-09 13:18:12 +0100

    Open Repository

---
+++ main.py
@@ -0,0 +1,10 @@
+class Menu:
+    def __init__():
+
+"""Un menu en début de partie est proposé au joueur permettant les actions
+suivantes :
+● Lancer une partie
+● Ajouter un Pokémon (dans un fichier texte nommé “pokemon.json”)
+● Accéder à son Pokedex."""
+
+# Créer un menu principal avec plusieurs options (Jouer, Ajouter un Pokémon, Pokedex, Quitter)
---
+++ pokedex.py
@@ -0,0 +1,5 @@
+class Pokedex:
+    def __init__():
+
+# Créer une méthode qui enregistre les Pokémon vaincus et les ajoute au Pokédex
+# Créer une méthode qui affiche tous les Pokémon du Pokédex

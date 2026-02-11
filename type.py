2aa89ed51fbef43eb49063166e35d207592999bd
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-02-11 09:07:11 +0100

    starting type dictionnary

---
+++ type.py
@@ -0,0 +1,26 @@
+class Type:
+    def __init__():
+        pass
+
+
+# Dictionnaire pour quel type bat/perd contre lequel.
+type_dict = {
+    "Bug": 
+    "Combat": 
+    "Darkness": 
+    "Dragon": 
+    "Electric": 
+    "Fairy":
+    "Fire":
+    "Fly":
+    "Ground":
+    "Ice":
+    "Normal":
+    "Plant":
+    "Poison":
+    "Psy":
+    "Rock":
+    "Specter": 
+    "Steel":
+    "Water":
+}
